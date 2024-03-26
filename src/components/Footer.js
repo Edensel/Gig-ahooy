5bb51ebb1f28728c678bb5044539e1214f9d103e
Kevin <kevin.kiseli@student.moringaschool.com>
2024-03-26 19:20:57 +0300

    Added contact links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 
+
 import React from "react";
 import "../style.css"; // Assuming your styles are here. Adjust if necessary.
 
@@ -6,9 +7,13 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2024 Gig Ahooy. All rights reserved.</p>
+        <p>© 2024 Gig Ahooy!!!. All rights reserved.</p>
         <p>Follow us on social media for updates and more!</p>
-
+        <div className="social-links">
+          <a href="https://twitter.com/example_twitter">Twitter</a> |
+          <a href="https://instagram.com/example_instagram"> Instagram</a> |
+          <a href="https://facebook.com/example"> Facebook</a>
+        </div>
       </div>
     </footer>
   );
